6ea8594472636965c98b38c73d93e2c95ec0394e
nxss1 <phouccinelli2@gmail.com>
2025-03-12 08:42:25 -0300

    released

---
+++ logica/4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() { //version 2.c float
+    float A;
+    float B;
+
+    scanf ("%f %f", &A, &B);
+
+    float X = A + B;
+
+    printf("Valor:%.2f", X);
+
+    return 0;
+}
