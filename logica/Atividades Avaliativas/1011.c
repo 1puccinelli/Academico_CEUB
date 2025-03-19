5511d1bea33518d1002a5a8c5cecf518dd81bfb0
nxss1 <phouccinelli2@gmail.com>
2025-03-19 08:55:55 -0300

    Raio e PI

---
+++ logica/Atividades Avaliativas/1011.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define PI  3.14159
+
+    int main() {
+
+        double raio, volume;
+
+        printf("Digite o valor do raio: ");
+        scanf("%lf", &raio);
+
+        volume = (4.0/3.0) * PI * (raio * raio * raio);
+
+        printf("Volume = %.3lf\n", volume);
+
+    return 0;
+
+    }
